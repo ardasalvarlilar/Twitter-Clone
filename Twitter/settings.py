9fb46fba6bce788dc3aaf950585d9ea69a92f8f7
Arda Şalvarlılar <arda4salvar7@gmail.com>
2023-12-20 19:14:44 +0300

    minor style changes

--- Twitter/settings.py
+++ Twitter/settings.py
@@ -21,15 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-wcv0x=c*e==!m)^(3nmq6rz#k=c@g0dgcwx@07c-=-(o13m!sw'
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-wcv0x=c*e==!m)^(3nmq6rz#k=c@g0dgcwx@07c-=-(o13m!sw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv('IS_DEVELOPMENT',True)
+DEBUG = True
 
-ALLOWED_HOSTS = [
-  getenv('APP_HOST')
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
