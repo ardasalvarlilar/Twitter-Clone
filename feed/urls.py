eb26a1fab43f375b3d785cee8388961db766b5bc
Arda Şalvarlılar <arda4salvar7@gmail.com>
2023-12-25 23:45:47 +0300

    debug

--- feed/urls.py
+++ feed/urls.py
@@ -3,7 +3,7 @@ from .views import *
 urlpatterns = [
   path('home/', home_view, name="home_page"),
   path('',home_view,name="home_page"),
-  path('/<slug:post_slug>/',post_detay_view,name="post_detay_page"),
+  path('<slug:post_slug>/',post_detay_view,name="post_detay_page"),
   path('gündemler/<slug:hashtag_slug>/', hashtag_detay_view,name="hashtag_detay_page"),
   path('post-detay/post-edit/<slug:post_slug>/',post_edit_view,name="post_edit_page"),
   path('post-detay/yorum-edit/<slug:yorum_slug>/',yorum_edit_view,name="yorum_edit_page"),
