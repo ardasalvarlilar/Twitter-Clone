ad26c034a0b8f76d258777d668687ef142f410b0
Arda Şalvarlılar <arda4salvar7@gmail.com>
2023-12-25 16:28:36 +0300

    test35

--- feed/urls.py
+++ feed/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
   path('home/', home_view, name="home_page"),
+  path('',home_view,name="home_page"),
   path('/<slug:post_slug>/',post_detay_view,name="post_detay_page"),
   path('gündemler/<slug:hashtag_slug>/', hashtag_detay_view,name="hashtag_detay_page"),
   path('post-detay/post-edit/<slug:post_slug>/',post_edit_view,name="post_edit_page"),
